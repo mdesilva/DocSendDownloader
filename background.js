7966218bd4de2f3fe506fd1074cadb4a72807619
Jean Weatherwax <jean.weatherwax@gmail.com>
2023-12-05 14:52:27 -0500

    Updates manifest.json for manifest version 3

--- background.js
+++ background.js
@@ -4,61 +4,39 @@ let jobComplete = false;
 
 const executeJob = () => {
     jobInProgress = true;
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const currentTabId = tabs[0].id;
-        chrome.tabs.executeScript(currentTabId, {file: "./modules/pdfkit.js"});
-        chrome.tabs.executeScript(currentTabId, {file: "./modules/blob-stream.js"});
-        chrome.tabs.executeScript(currentTabId, {file: "./src/ModifyDocSendView.js"});
-        chrome.tabs.executeScript(currentTabId, {file: "./src/GeneratePDF.js"});
-        chrome.tabs.executeScript(currentTabId, {file: "./src/DocSendDownloader.js"}, () => {
+        chrome.scripting.executeScript({ target: { tabId: currentTabId }, files: ["./modules/pdfkit.js"] });
+        chrome.scripting.executeScript({ target: { tabId: currentTabId }, files: ["./modules/blob-stream.js"] });
+        chrome.scripting.executeScript({ target: { tabId: currentTabId }, files: ["./src/ModifyDocSendView.js"] });
+        chrome.scripting.executeScript({ target: { tabId: currentTabId }, files: ["./src/GeneratePDF.js"] });
+        chrome.scripting.executeScript({ target: { tabId: currentTabId }, files: ["./src/DocSendDownloader.js"] }, () => {
             connection = chrome.tabs.connect(currentTabId);
-            connection.postMessage({requestType: "GENERATE_PDF"});
+            connection.postMessage({ requestType: "GENERATE_PDF" });
             connection.onMessage.addListener((message) => {
                 if (message.requestType == "SET_JOB_COMPLETE") {
                     jobInProgress = false;
                     jobComplete = true;
                 }
-            })
-        })
-    })
-}
+            });
+        });
+    });
+};
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostSuffix: 'docsend.com', pathContains: 'view'},
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+    // Placeholder for any setup code needed on installation
+    console.log('Extension installed');
 });
 
-
-chrome.pageAction.onClicked.addListener(() => {
-    
-    chrome.webRequest.onHeadersReceived.addListener(
-        function(response) {
-            response.responseHeaders.push({'name': "Access-Control-Allow-Origin", 'value': "*"});
-            response.responseHeaders.push({'name': "Access-Control-Allow-Methods", 'value': "GET, OPTIONS"});
-            return {responseHeaders: response.responseHeaders}
-        },
-        {
-            urls: ["https://*.docsend.com/*", "https://*.cloudfront.net/*"]
-        },
-        ["blocking", "responseHeaders", "extraHeaders"]
-    )
-
+chrome.action.onClicked.addListener(() => {
     if (jobComplete || jobInProgress) {
         try {
-            connection.postMessage({requestType: "CHECK_PROGRESS"});
-        } 
-        catch {
-            //Connection closed, start new job
+            connection.postMessage({ requestType: "CHECK_PROGRESS" });
+        } catch {
+            // Connection closed, start new job
             executeJob();
         }
-    }
-    else if (!jobInProgress && !jobComplete) {
+    } else if (!jobInProgress && !jobComplete) {
         executeJob();
     }
-})
+});
