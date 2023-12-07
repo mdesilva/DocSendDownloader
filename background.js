b82820226492fd2b2065169403550c907eabbd9f
Jean Weatherwax <jean.weatherwax@gmail.com>
2023-12-07 11:03:49 -0700

    Use fetchResource custom method instead of fetch for images

--- background.js
+++ background.js
@@ -65,3 +65,18 @@ chrome.action.onClicked.addListener(() => {
         executeJob();
     }
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    fetch(request.input, request.init).then(function(response) {
+      return response.text().then(function(text) {
+        sendResponse([{
+          body: text,
+          status: response.status,
+          statusText: response.statusText,
+        }, null]);
+      });
+    }, function(error) {
+      sendResponse([null, error]);
+    });
+    return true;
+  });
--- src/GeneratePDF.js
+++ src/GeneratePDF.js
@@ -3,8 +3,27 @@ let numSlidesComplete = 0;
 const doc = new PDFDocument({layout:'landscape', margin: 0, autoFirstPage: false});
 const stream = doc.pipe(blobStream());
 
+function fetchResource(input, init) {
+    return new Promise((resolve, reject) => {
+      chrome.runtime.sendMessage({input, init}, messageResponse => {
+        const [response, error] = messageResponse;
+        console.log(response, "response in fetchResource")
+        if (response === null) {
+          reject(error);
+        } else {
+          // Use undefined on a 204 - No Content
+          const body = response.body ? new Blob([response.body]) : undefined;
+          resolve(new Response(body, {
+            status: response.status,
+            statusText: response.statusText,
+          }));
+        }
+      });
+    });
+  }
+
 const getImageAsBlob = async (url) => 
-    await fetch(url)
+    await fetchResource(url)
     .then((response) =>{
         console.log(response, "getImageAsBlob response")
         numSlidesComplete++;
@@ -26,7 +45,7 @@ const getImageAsBlob = async (url) =>
         for (let i=0; i<imageUrls.length; i++) {
             await getImageAsBlob(imageUrls[i]).then(data => {
                 console.log(imageUrls[i], "imageUrls[i]")
-                console.log(data, "data")
+                console.log(data, "data in addSlidesToPDF")
                 const img = doc.openImage(data);
                 console.log(img.width, img.height, img);
                 doc.addPage({size: [img.width, img.height]});
